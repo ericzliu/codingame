9034f71c2221d57298cd62d5788f509beff73e67
Zhi LIU <liuzhi0210@gmail.com>
2018-05-10 23:07:49 +0200

    Add formatting

--- cgx-formatter/index.js
+++ cgx-formatter/index.js
@@ -1,3 +1,5 @@
+const EOL = '\r\n';
+
 const TokenType = Object.freeze({
     UNKNOWN: 0, // Usefule if the token type is defined later than calling the token constructor
     SEMICOLON: 1,
@@ -91,8 +93,8 @@ class Scanner {
             }
             if (char != APOSTROPHE) throw new Error(`Can't find apostrophe ${APOSTROPHE}.`);
             buffer.push(char);
-            return new Token(TokenType.STRING, buffer);
-        } else if (char === 't') {
+            return new Token(TokenType.STRING, buffer.join(''));
+        } else if (char === 't' || char === 'f') {
             const buffer = [];
             while (isBoolean(char)) {
                 buffer.push(char);
@@ -100,7 +102,7 @@ class Scanner {
             }
             // To stop the pointer at last char of last token
             this.prevChar();
-            return new Token(TokenType.BOOL, buffer);
+            return new Token(TokenType.BOOL, buffer.join(''));
         } else if (isNumber(char)) {
             const buffer = [];
             while (isNumber(char)) {
@@ -108,7 +110,7 @@ class Scanner {
                 char = this.nextChar();
             }
             this.prevChar();
-            return new Token(TokenType.NUMBER, buffer);
+            return new Token(TokenType.NUMBER, buffer.join(''));
         } else {
             throw new Error(`Unrecognised character ${char}`);
         }
@@ -127,9 +129,240 @@ class Scanner {
     }
 }
 
+function whitespaces(indent) {
+    var str = '';
+    for (var i = 0; i < indent; i += 1) {
+        str = str.concat(' ');
+    }
+    return str;
+}
+
+class Element {
+    toString() {
+        return '';
+    }
+
+    format(indent) {
+        return '';
+    }
+}
+
+class Primitive extends Element {
+    constructor(token) {
+        super();
+        this.token = token;
+    }
+
+    toString() {
+        let str = '';
+        switch (this.token.type) {
+            case TokenType.NUMBER:
+                str = this.token.chars;
+                break;
+            case TokenType.BOOL:
+                str = this.token.chars;
+                break;
+            case TokenType.STRING:
+                str = this.token.chars;
+                break;
+        }
+        return str;
+    }
+
+    format(indent) {
+        let str = whitespaces(indent);
+        str = str.concat(this.toString());
+        return str;
+    }
+}
+
+class NullElement extends Primitive {
+    constructor() {
+        super(undefined);
+    }
+
+    toString() {
+        return '';
+    }
+
+    format(indent) {
+        return '';
+    }
+}
+
+class Block extends Element {
+    constructor(elements) {
+        super();
+        this.elements = elements;
+    }
+
+    toString() {
+        let str = '(';
+        let isFirst = true;
+        for (let element of this.elements) {
+            if (isFirst) {
+                isFirst = false;
+            } else {
+                str += ';';
+            }
+            str += element.toString();
+        }
+        str += ')';
+        return str;
+    }
+
+    format(indent) {
+        let str = whitespaces(indent) + '(' + EOL;
+        const childIndent = indent + 4;
+        let isFirst = true;
+        for (var i = 0; i < this.elements.length; i += 1) {
+            const element = this.elements[i];
+            str = str.concat(element.format(childIndent));
+            if (i < (this.elements.length - 1)) {
+                str = str.concat(';');
+            }
+            str = str.concat(EOL);
+        }
+        str = str.concat(whitespaces(indent) + ')');
+        return str;
+    }
+}
+
+class KeyValue extends Element {
+    constructor(key, value) {
+        super();
+        this.key = key;
+        this.value = value;
+    }
+
+    toString() {
+        let str = this.key.toString();
+        str += '=';
+        str += this.value.toString();
+        return str;
+    }
+
+    format(indent) {
+        let str = whitespaces(indent);
+        str = str.concat(this.key.toString() + '=');
+        if (this.value instanceof Block) {
+            str = str.concat(EOL);
+            str = str.concat(this.value.format(indent));
+        } else if (this.value instanceof NullElement) {
+
+        } else if (this.value instanceof Primitive) {
+            str = str.concat(this.value.toString());
+        }
+        return str;
+    }
+}
+
+function parseElement(scanner) {
+    let token = scanner.next();
+    if (isNullOrUndefined(token)) {
+        return new NullElement();
+    }
+    if (token.type === TokenType.OPEN_PAREN) {
+        scanner.prev();
+        return parseBlock(scanner);
+    } else if (token.type === TokenType.STRING) {
+        let suiv = scanner.next();
+        if (!isNullOrUndefined(suiv) && suiv.type === TokenType.ASSIGN) {
+            scanner.prev();
+            scanner.prev();
+            return parseKeyValue(scanner);
+        } else {
+            if (isNullOrUndefined(suiv)) {
+                scanner.prev();
+            } else {
+                scanner.prev();
+                scanner.prev();
+            }
+            return parsePrimitive(scanner);
+        }
+    } else {
+        scanner.prev();
+        return parsePrimitive(scanner);
+    }
+}
+
+function parseBlock(scanner) {
+    const open = scanner.next();
+    if (open.type === TokenType.OPEN_PAREN) {
+        const elements = [];
+        while (true) {
+            let token = scanner.next();
+            if (token.type === TokenType.CLOSE_PAREN) {
+                return new Block(elements);
+            } else {
+                scanner.prev();
+                let element = parseElement(scanner);
+                if (isNullOrUndefined(element)) {
+                    return;
+                }
+                if (!(element instanceof NullElement)) {
+                    elements.push(element);
+                }
+                token = scanner.next();
+                if (token.type === TokenType.SEMICOLON) {
+                    continue;
+                } else if (token.type === TokenType.CLOSE_PAREN) {
+                    return new Block(elements);
+                } else {
+                    throw new Error('Syntax error in block');
+                }
+            }
+        }
+    }
+}
+
+function parsePrimitive(scanner) {
+    const token = scanner.next();
+    let isPrimitive = (token.type === TokenType.NUMBER);
+    isPrimitive = isPrimitive || (token.type === TokenType.BOOL);
+    isPrimitive = isPrimitive || (token.type === TokenType.STRING);
+    if (isPrimitive) {
+        return new Primitive(token);
+    } else {
+        scanner.prev();
+        return new NullElement();
+    }
+}
+
+function parseKeyValue(scanner) {
+    const key = scanner.next();
+    if (key.type === TokenType.STRING) {
+        const assign = scanner.next();
+        if (assign.type === TokenType.ASSIGN) {
+            const token = scanner.next();
+            if (isNullOrUndefined(token)) {
+                return new KeyValue(new Primitive(key), new NullElement());
+            }
+            if (token.type === TokenType.OPEN_PAREN) {
+                scanner.prev();
+                const block = parseBlock(scanner);
+                return new KeyValue(new Primitive(key), block);
+            } else {
+                scanner.prev();
+                const primitive = parsePrimitive(scanner);
+                return new KeyValue(new Primitive(key), primitive);
+            }
+        }
+    }
+}
+
 module.exports = {
     Scanner: Scanner,
     Token: Token,
     TokenType: TokenType,
-    isNullOrUndefined: isNullOrUndefined
+    isNullOrUndefined: isNullOrUndefined,
+    Element: Element,
+    Primitive: Primitive,
+    NullElement: NullElement,
+    Block: Block,
+    KeyValue: KeyValue,
+    parseElement: parseElement,
+    parseBlock: parseBlock,
+    parsePrimitive: parsePrimitive,
+    parseKeyValue: parseKeyValue
 };
--- cgx-formatter/test.js
+++ cgx-formatter/test.js
@@ -15,6 +15,21 @@ function readInput(lines) {
     }
 }
 
+function readAnswer(path) {
+    return new Promise((resolve, reject) => {
+        var data = '';
+        var rd = fs.createReadStream(path, 'utf8');
+        var isFirst = true;
+        rd.on('data', function (chunk) {
+            data += chunk;
+        }).on('end', function () {
+            resolve(data);
+        }).on('error', function (error) {
+            reject(error);
+        });
+    });    
+}
+
 function readFile(path) {
     return new Promise((resolve, reject) => {
         var data = '';
@@ -33,7 +48,7 @@ function readFile(path) {
 
 describe('Smaller CGX compiler', () => {
     it('scanner next/prev behaves like i++, i--', () => {
-        readFile('testcases/block_containing_a_single_value').then(lines => {
+        return readFile('testcases/block_containing_a_single_value').then(lines => {
             let scanner = new app.Scanner(lines);
             let token = scanner.next();
             assert.equal(app.TokenType.OPEN_PAREN, token.type);
@@ -61,7 +76,7 @@ describe('Smaller CGX compiler', () => {
     });
 
     it('scanner parse block with multiple values', () => {
-        readFile('testcases/block_containing_multiple_values').then(lines => {
+        return readFile('testcases/block_containing_multiple_values').then(lines => {
             let scanner = new app.Scanner(lines);
             scanner.next(); // (
             let zero = scanner.next();
@@ -72,6 +87,158 @@ describe('Smaller CGX compiler', () => {
             assert.equal('0', zero.chars);
             assert.equal('1', one.chars);
             assert.equal('2', two.chars);
+        });
+    });
+
+    it('scanner parse simple string', () => {
+        return readFile('testcases/simple_string_of_characters_which_must_not_be_modified').then(lines => {
+            let scanner = new app.Scanner(lines);
+            let token = scanner.next();
+            assert.equal(token.type, app.TokenType.STRING);
+            assert.deepEqual(token.chars, "' Content with spaces and	tabs'");
+        });
+    });
+
+    it('parser can parse primitive string', () => {
+        return readFile('testcases/simple_string_of_characters_which_must_not_be_modified').then(lines => {
+            let scanner = new app.Scanner(lines);
+            let element = app.parseElement(scanner);
+            assert.isTrue(element instanceof app.Primitive);
+            assert.deepEqual(element.token.chars, "' Content with spaces and	tabs'");
+        });
+    });
+
+    it('parser can parse nested blocks', () => {
+        return readFile('testcases/nested_blocks').then(lines => {
+            const scanner = new app.Scanner(lines);
+            const element = app.parseElement(scanner);
+            assert.isTrue(element instanceof app.Block);
+            const elements = element.elements;
+            assert.isTrue(elements[0] instanceof app.Block);
+            assert.equal(elements[0].elements[0].token.type, app.TokenType.BOOL);
+        });
+    });
+
+    it('parser can parse empty blocks', () => {
+        return readFile('testcases/empty_block').then(lines => {
+            const scanner = new app.Scanner(lines);
+            const element = app.parseElement(scanner);
+            assert.isTrue(element instanceof app.Block);
+            const elements = element.elements;
+            assert.equal(elements.length, 0);
+        });
+    });
+
+    it('parser can parse block_with_several_key_value', () => {
+        return readFile('testcases/block_with_several_key_value').then(lines => {
+            const scanner = new app.Scanner(lines);
+            const element = app.parseElement(scanner);
+            assert.isTrue(element instanceof app.Block);
+            const elements = element.elements;
+            assert.equal(elements.length, 4);
+            assert.isTrue(elements[0] instanceof app.KeyValue);
+            assert.isTrue(elements[1] instanceof app.KeyValue);
+            assert.isTrue(elements[2] instanceof app.Primitive);
+            assert.isTrue(elements[3] instanceof app.KeyValue);
+            assert.equal(elements[0].toString(), "'k1'='v1'");
+            assert.equal(elements[2].toString(), '123');
+        });
+    });
+
+    it('parser can parse example_provided', () => {
+        return readFile('testcases/example_provided').then(lines => {
+            const scanner = new app.Scanner(lines);
+            const element = app.parseElement(scanner);
+            assert.isTrue(element instanceof app.KeyValue);
+            assert.isTrue(element.value instanceof app.Block);
+            const value = element.value;
+            assert.equal(value.elements.length, 3);
+            const one = value.elements[0];
+            const two = value.elements[1];
+            const three = value.elements[2];
+            assert.isTrue(one instanceof app.Block);
+            assert.equal(one.elements.length, 3);
+            assert.isTrue(one.elements[2] instanceof app.KeyValue);
+            assert.isTrue(one.elements[2].value instanceof app.Block);
+            const str = element.toString();
+            assert.equal(str, "'users'=(('id'=10;'name'='Serge';'roles'=('visitor';'moderator'));('id'=11;'name'='Biales');true)");
+        });
+    });
+
+    it('format block_containing_a_single_value.out', () => {
+        return readFile('testcases/block_containing_a_single_value').then(lines => {
+            const scanner = new app.Scanner(lines);
+            const element = app.parseElement(scanner);
+            return readAnswer('testcases/block_containing_a_single_value.out').then(data => {
+                const output = element.format(0);
+                assert.equal(output, data.trim());
+            });
+        });
+    });
+    
+    it('format block_containing_multiple_values', () => {
+        return readFile('testcases/block_containing_multiple_values').then(lines => {
+            const scanner = new app.Scanner(lines);
+            const element = app.parseElement(scanner);
+            return readAnswer('testcases/block_containing_multiple_values.out').then(data => {
+                const output = element.format(0);
+                assert.equal(output, data.trim());
+            });
+        });
+    });
+    
+    it('format block_with_several_key_value', () => {
+        return readFile('testcases/block_with_several_key_value').then(lines => {
+            const scanner = new app.Scanner(lines);
+            const element = app.parseElement(scanner);
+            return readAnswer('testcases/block_with_several_key_value.out').then(data => {
+                const output = element.format(0);
+                assert.equal(output, data.trim());
+            });
+        });
+    });
+
+    it('format empty_block', () => {
+        return readFile('testcases/empty_block').then(lines => {
+            const scanner = new app.Scanner(lines);
+            const element = app.parseElement(scanner);
+            return readAnswer('testcases/empty_block.out').then(data => {
+                const output = element.format(0);
+                assert.equal(output, data.trim());
+            });
+        });
+    });
+
+    it('format example_provided', () => {
+        return readFile('testcases/example_provided').then(lines => {
+            const scanner = new app.Scanner(lines);
+            const element = app.parseElement(scanner);
+            return readAnswer('testcases/example_provided.out').then(data => {
+                const output = element.format(0);
+                assert.equal(output, data.trim());
+            });
+        });
+    });
+
+    it('format full_example', () => {
+        return readFile('testcases/full_example').then(lines => {
+            const scanner = new app.Scanner(lines);
+            const element = app.parseElement(scanner);
+            return readAnswer('testcases/full_example.out').then(data => {
+                const output = element.format(0);
+                assert.equal(output, data.trim());
+            });
+        });
+    });
+
+    it('format numerous_overlaps', () => {
+        return readFile('testcases/numerous_overlaps').then(lines => {
+            const scanner = new app.Scanner(lines);
+            const element = app.parseElement(scanner);
+            return readAnswer('testcases/numerous_overlaps.out').then(data => {
+                const output = element.format(0);
+                assert.equal(output, data.trim());
+            });
         });
     });
 });
