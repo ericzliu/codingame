a08373f6c3cac22fe334be661ea8035846a21f80
Zhi LIU <liuzhi0210@gmail.com>
2018-05-31 08:28:58 +0200

    labyrinth 87 percent done

--- the-labyrinth/labyrinth.py
+++ the-labyrinth/labyrinth.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import sys
-from collections import deque
+import heapq
+import math
 
 class Node(object):
 
@@ -81,114 +82,160 @@ def get_direction(src_pos, dst_pos):
     else:
         raise Exception('Wrong direction')
 
-def all_children(pos, maze, old_pos):
+def all_neighbors(pos, maze, visted_set):
     kr = pos[0]
     kc = pos[1]
     all_pos = [(kr - 1, kc), (kr + 1, kc), (kr, kc + 1), (kr, kc - 1)]
-    return [p for p in all_pos if not(p in old_pos) and not(maze.is_out(p)) and not(maze.is_wall(p)) and not(maze.is_unknown(p))]
+    return [p for p in all_pos if not(p in visted_set) and not(maze.is_out(p)) and not(maze.is_wall(p)) and not(maze.is_unknown(p))]
 
-def find_path_bfs(src, dst, visited):
-    final = None # last search node in the path search
-    fifo_queue = deque()
-    fifo_queue.append(Node(src))
-    visited[src] = True
-    while len(fifo_queue) > 0 and (final is None):
-        node = fifo_queue.popleft()
-        if dst == node.position:
-            final = node
-        else:
-            subnodes = [Node(x) for x in all_children(node.position, maze, visited)]
-            for sub_node in subnodes:
-                sub_node.parent = node
-            if len(subnodes) > 0:
-                fifo_queue.extend(subnodes)
-    if final is None:
-        raise Exception('No path')
-    rev = []
-    while not (final is None):
-        rev.append(final.position)
-        final = final.parent
-    return list(reversed(rev))
-
-def trace(node):
-    ans = []
-    while node is not None:
-        ans.append(node.position)
-        node = node.parent
-    return list(reversed(ans))
-
-def find_path_dfs(src, dst, visited):
-    final = None # last search node in the path search
-    lifo_queue = deque()
-    lifo_queue.append(Node(src))
-    visited[src] = True
-    while len(lifo_queue) > 0 and (final is None):
-        node = lifo_queue.pop()
-        if dst == node.position:
-            final = node
-        else:
-            subnodes = [Node(x) for x in all_children(node.position, maze, visited)]
-            for sub_node in subnodes:
-                sub_node.parent = node
-            if len(subnodes) > 0:
-                lifo_queue.extend(subnodes)
-    if final is None:
-        raise Exception('No path')
-    return trace(final)
-
-if True:
-    r, c, a = [int(i) for i in input().split()]
+# row, col, f, g, h, parent
     
-    maze = Maze(r, c)
-    old_pos = {}
-    curr = None # the search leaf node before control room is known
-    is_control_room_seen = False
-    is_go = True
-    path_go = None
-    path_back = None
-
-    while True:
-        kr, kc = [int(i) for i in input().split()]
-        input_pos = (kr, kc)
-        for i in range(r):
-            row = input()
-            maze.update_row(i, row)
+class Elt(object):
+    
+    def __init__(self, row, col):
+        self.row = row
+        self.col = col
+        self.f = math.inf
+        self.g = math.inf
+        self.h = math.inf
+        self.parent = None
         
-        print(input_pos, file=sys.stderr)
-        print((not is_control_room_seen) and maze.is_control_room_seen(), file=sys.stderr)
-        if (not is_control_room_seen) and maze.is_control_room_seen():
-            is_control_room_seen = True
-            print('control room seen', file=sys.stderr)
-            # print(str(curr.__dict__), file=sys.stderr)
-            path_go = trace(curr) + find_path_bfs(input_pos, maze.control_room, old_pos)[1:]
-            print(path_go, file=sys.stderr)
+    def __eq__(self, other):
+        return (self.f, self.g, self.h, self.row, self.col) == (other.f, other.g, other.h, other.row, other.col)
 
-        if is_control_room_seen:
-            if is_go and input_pos == maze.control_room:
-                is_go = False
-            if not is_go and path_back is None:
-                path_back = find_path_bfs(maze.control_room, maze.starting_point, {})
-                # TODO: USE A FASTER PATH FINDING ALGORITHM
+    def __lt__(self, other):
+        return (self.f, self.g, self.h, self.row, self.col) < (other.f, other.g, other.h, other.row, other.col)
+        
+    @property
+    def g(self):
+        return self.__g
+    
+    @g.setter
+    def g(self, value):
+        self.__g = value
+        if self.g < math.inf and self.h < math.inf:
+            self.f = self.g + self.h
+    
+    @property
+    def h(self):
+        return self.__h
 
-            path = path_go if is_go else path_back
-            print(get_direction(input_pos, path[path.index(input_pos) + 1]))
-        else:
-            # before seeing the control room, discover the map using dfs and backtracing
-            if curr is None:
-                curr = Node(input_pos)
-            old_pos[input_pos] = True
+    @h.setter
+    def h(self, value):
+        self.__h = value
+        if self.g < math.inf and self.h < math.inf:
+            self.f = self.g + self.h
+    
+class OpenSet(object):
 
-            lst_child = all_children(input_pos, maze, old_pos)
-            if len(lst_child) > 0:
-                dst = next((x for x in lst_child if maze.is_control_room(x)), lst_child[0])
-                print(dst, file=sys.stderr)
-                direction = get_direction(input_pos, dst)
-                parent = curr
-                child = Node(dst)
-                child.parent = parent
-                curr = child
+    def __init__(self):
+        self.positions = set() #
+        self.heap = []
+    
+    def pop_elt(self):
+        while True:
+            _, elt = heapq.heappop(self.heap)
+            if (elt.row, elt.col) in self.positions:
+                self.positions.remove((elt.row, elt.col))
+                return elt
+
+    def remove(self, elt):
+        self.positions.remove((elt.row, elt.col))
+    
+    def add(self, elt):
+        self.positions.add((elt.row, elt.col))
+        heapq.heappush(self.heap, (elt.f, elt))
+    
+    def is_empty(self):
+        return len(self.positions) == 0
+
+def find_path(maze, src, dest, alarm):
+    
+    def heuristic(src, dst):
+        md = math.fabs(src[0] - dst[0]) + math.fabs(src[1] - dst[1])
+        return md
+    
+    def construct_path(elt):
+        rev = []
+        while elt is not None:
+            rev.append((elt.row, elt.col))
+            elt = elt.parent
+        return list(reversed(rev))
+
+    closed_set = set()
+    open_set = OpenSet()
+    src_elt = Elt(src[0], src[1])
+    src_elt.g = 0
+    src_elt.h = heuristic(src, dest)
+    open_set.add(src_elt)
+    while not open_set.is_empty():
+        elt = open_set.pop_elt()
+        print((elt.row, elt.col), file=sys.stderr)
+        if elt.row == dest[0] and elt.col == dest[1]:
+            p = construct_path(elt)
+            print(str(len(p)) + ' alarm ' + str(alarm), file=sys.stderr)
+            if len(p) <= (alarm + 1):
+                return p
             else:
-                # go back to parent
-                direction = get_direction(input_pos, curr.parent.position)
-                curr = curr.parent
-            print(direction)
+                pass
+        else:    
+            closed_set.add((elt.row, elt.col))
+            
+            neighbors = all_neighbors((elt.row, elt.col), maze, closed_set)
+            for neighbor in neighbors:
+                neighbor_elt = Elt(neighbor[0], neighbor[1])
+                neighbor_elt.g = elt.g + 1
+                neighbor_elt.h = heuristic(neighbor, dest)
+                neighbor_elt.parent = elt
+                open_set.add(neighbor_elt)
+
+r, c, a = [int(i) for i in input().split()]
+
+maze = Maze(r, c)
+old_pos = set()
+curr = None # the search leaf node before control room is known
+is_go = True
+path_go = None
+path_back = None
+
+while True:
+    kr, kc = [int(i) for i in input().split()]
+    input_pos = (kr, kc)
+    for i in range(r):
+        row = input()
+        maze.update_row(i, row)
+    
+    # print(input_pos, file=sys.stderr)
+    if is_go and maze.is_control_room_seen() and path_go is None:
+        path_go = find_path(maze, input_pos, maze.control_room, math.inf)
+        print(path_go, file=sys.stderr)
+
+    if (is_go and path_go is not None) or (not is_go):
+        if is_go and input_pos == maze.control_room:
+            is_go = False
+        if not is_go and path_back is None:
+            path_back = find_path(maze, maze.control_room, maze.starting_point, a)
+            print(path_back, file=sys.stderr)
+
+        path = path_go if is_go else path_back
+        print(get_direction(input_pos, path[path.index(input_pos) + 1]))
+    else:
+        # before seeing the control room, discover the map using dfs and backtracing
+        if curr is None:
+            curr = Node(input_pos)
+        old_pos.add(input_pos)
+
+        lst_child = all_neighbors(input_pos, maze, old_pos)
+        if len(lst_child) > 0:
+            dst = next((x for x in lst_child if maze.is_control_room(x)), lst_child[0])
+            # print(dst, file=sys.stderr)
+            direction = get_direction(input_pos, dst)
+            parent = curr
+            child = Node(dst)
+            child.parent = parent
+            curr = child
+        else:
+            # go back to parent
+            direction = get_direction(input_pos, curr.parent.position)
+            curr = curr.parent
+        print(direction)
